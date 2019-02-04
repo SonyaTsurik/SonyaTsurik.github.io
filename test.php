b7a45ad0f392129d2d5b2d762341021971f6eae9
SonyaTsurik <46749116+SonyaTsurik@users.noreply.github.com>
2019-02-04 16:43:09 +0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<head>
+	<link rel="stylesheet" type="text/css" href="stylle.css">
+<title>Резюме</title>
+</head>
+<body>
+	<header>
+		<div class="header__logo"><p>резюме</p></div>
+		<nav>
+			<div class="topnav" id="mytopnav" >
+				<a href="Sonyadz.html">Главная</a> 
+               <a href="">Обо мне</a>
+           </div>
+	  	
+           </nav>
+       </header>
+
+<style type="text/css">
+			*{
+			margin: 0;
+			padding: 0;
+		}
+		a{
+			text-decoration: none;
+		}
+ 
+	header {
+				width: 100%;
+				background: #000;
+				display: flex;
+			}
+			
+			.image_position {
+				border-left: 20px;
+				margin-left: 200px; 
+			}
+			
+			.header__logo
+			 {
+			 	float: left;
+				text-transform: uppercase;
+				color: #fff;
+				font-weight: 800;
+				font-size: 25px;
+				cursor: pointer;
+				padding-top: 20px;
+				padding-left: 12%;
+				padding-bottom: 20px;
+			 }
+			nav {
+				padding-top: 20px;
+				padding-left: 30%;
+				text-transform: uppercase;
+				color: #fff;
+				font-weight: 800;
+			 
+				cursor: pointer;
+			}  
+	 .topnav a {
+	 	color: #fff;
+	 	text-align: center;
+	 	padding: 12px 16px;
+	 	font-size: 15px;
+	 	font-weight: bold;
+	 }
+.topnav a:hover{
+	border-bottom: 3px solid navy;
+}
+</style>
